3f2b67c5ebabb364c7f4dd1ebf27118bd3b54428
zhzkdls <gkdud3558@naver.com>
2022-07-31 20:30:27 +0900

    add

--- src/js/login.js
+++ src/js/login.js
@@ -1,16 +1,20 @@
-const loginInput = document.querySelector("#login-form input");
-const loginForm = document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form");
 
-const link = document.querySelector("a")
+const loginInput = document.querySelector("#login-form input");
+
+const greeting = document.querySelector("#greeting");
+
+
+const HIDDEN_CLASSNAME = "hidden";
 
   function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    
+    greeting.innerText = `안녕 ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
   }
 
-  function handleLinkClick(event) {
-    alert("Clicked!!");
-  } 
 
-loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+loginForm.addEventListener("submit", onLoginSubmit);
