81935f1e68f3a24881bd12fa93d56c2c69a26af5
zhzkdls <gkdud3558@naver.com>
2022-08-06 23:18:19 +0900

    add

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,24 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const video = ["1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenVideo = video[Math.floor(Math.random() * video.length)];
+const bgVideo = document.querySelector(".bg_video");
+const bgVideoSrc = document.querySelector(".bg_video source");
 
-const bgImage = document.createElement("img");
 
-bgImage.src = `src/img/${chosenImage}`;
+bgVideoSrc.src = `src/video/${chosenVideo}`;
+bgVideo.appendChild(bgVideoSrc);
 
-document.body.appendChild(bgImage);
+
+
+
+
+
+
+
+// const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// const bgImage = document.createElement("img");
+
+// bgImage.src = `src/video/${chosenImage}`;
+
+// document.body.appendChild(bgImage);
