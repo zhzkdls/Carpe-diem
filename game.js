9a152099e4a2810a281de61ad5cb67cc3151f819
zhzkdls <gkdud3558@naver.com>
2022-08-09 23:40:47 +0900

    add

--- game.js
+++ game.js
@@ -1,22 +1,18 @@
 const subtitle = document.querySelector(".game_subtitle");
 const num = document.querySelector(".num_go");
 
-// 선택한 숫자 + 랜덤 숫자
 const textSelect = document.querySelector(".selection span");
 const textResult = document.querySelector(".result span");
 
-// 결과를 나타내는 글자(3)
 const game_form = document.querySelector(".game_form");
 const formInput = game_form.querySelector(".game_input");
 
-// 0 ~ 50까지 숫자 설정
 function handleRange() {
     const maxRange = num.value;
     formInput.value = "";
     formInput.setAttribute("max", maxRange);
 }
 
-// 랜덤 숫자 생성
 function createRandomNumber() {
     const randomRange = parseInt(num.value) + 1;
     const number = Math.floor(Math.random() * randomRange);
