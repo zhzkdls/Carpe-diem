7a815759a9d2f9f905536c1e7c8c16cacea66fd1
zhzkdls <gkdud3558@naver.com>
2022-08-10 23:30:27 +0900

    add

--- game.js
+++ game.js
@@ -1,45 +1,33 @@
-const subtitle = document.querySelector(".game_subtitle");
-const num = document.querySelector(".num_go");
+// (01) querySelector을 사용해 id 값으로 찾은 h2 요소를 변수에 저장해준다.
 
-const textSelect = document.querySelector(".selection span");
-const textResult = document.querySelector(".result span");
+const remainTime = document.querySelector("#remain-time");
 
-const game_form = document.querySelector(".game_form");
-const formInput = game_form.querySelector(".game_input");
+// (02) 함수를 만들고 2022년 12월 25일 기준과 현재를 기준으로 변수 2개를 만든다.
 
-function handleRange() {
-    const maxRange = num.value;
-    formInput.value = "";
-    formInput.setAttribute("max", maxRange);
+// (03) diff 변수를 두어 위에서 선언한 2개의 변수를 빼준다.
+
+// (04) diff 변수를 날짜, 시간, 분, 초에 맞게 나눠준 후에 Math.floor()을 사용한다.
+
+// (설명) 위에서 언급했듯이 "밀리세컨드 단위" 로 출력이 된다. 그럼 우리는 밀리세컨드 단위를 사용해 날짜, 시간, 분, 초를 가지고 와야 한다. 밀리세컨드 단위이기 때문에 1초는 1*1000이라고 할수 있다. 우선 diffDay를 살펴보자. 날짜를 나타내려면 밀리세컨드 초를 60초, 06분, 24시간으로 나눠주면 된다. 
+
+// (05) 아까 가져온 h2 요소에 .innerText를 사용해 추가해준다.
+
+// (06) setInterval()을 사용해 1초 간격으로 지속적으로 함수를 호출한다. 
+// -> 앞에 함수를 호출한 이유는 브라우저가 js파일을 호출했을때, setInterval 실행 전의 딜레이를 없애기 위함이다.
+
+function diffDay() {
+    const masTime = new Date("2022-12-25");
+    const todayTime = new Date();
+
+    const diff = masTime - todayTime;
+
+    const diffDay = Math.floor(diff / (1000*60*60*24));
+    const diffHour = Math.floor((diff / (1000*60*60)) % 24);
+    const diffMin = Math.floor((diff / (1000*60)) % 60);
+    const diffSec = Math.floor(diff / (1000%60));
+
+    remainTime.innerText = `${diffDay}일 ${diffHour}시간 ${diffMin}분 ${diffSec}초`;
 }
 
-function createRandomNumber() {
-    const randomRange = parseInt(num.value) + 1;
-    const number = Math.floor(Math.random() * randomRange);
-    return number;
-}
-
-function textSelectHeading(input, answer) {
-    textSelect.innerText = `You chose ${input}, the machine chose ${answer}.`;
-}
-
-function textResultHeading(input, answer) {
-    if (parseInt(input) === parseInt(answer)) {
-        textResult.innerText = "You Win!";
-    } else 
-        textResult.innerText = "You Lost!";
-    }
-
-function handleSubmit(event) {
-    event.preventDefault();
-    const input = formInput.value;
-    const answer = createRandomNumber();
-    textSelectHeading(input, answer);
-    textResultHeading(input, answer);
-}
-
-function init() {
-    num.addEventListener("input", handleRange);
-    game_form.addEventListener("submit", handleSubmit);
-}
-init();
+diffDay();
+setInterval(diffDay, 1000);
