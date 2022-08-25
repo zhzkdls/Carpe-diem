3a5ee09a0173c57d880e2c375c02a265c6d1272a
zhzkdls <gkdud3558@naver.com>
2022-08-25 14:39:12 +0900

    add

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const numberBtns = document.querySelectorAll("[data-number");
+const operatroBtns = document.querySelectorAll("[data-operator");
+const equals = document.querySelectorAll("[data-equals");
+const percent = document.querySelectorAll("[ata-percent");
+const clear = document.querySelectorAll("[data-clear");
+const previousOperand = document.querySelectorAll("[data-previous-operand");
+const currentOperand = document.querySelectorAll("[data-current-operand");
